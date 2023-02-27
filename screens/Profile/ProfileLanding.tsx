0c25fb36bf8e7c148578faa52e23d6aa98006169
cjlaflamme1 <chadlaflamme@gmail.com>
2023-02-27 18:14:05 -0500

    image selector button

--- screens/Profile/ProfileLanding.tsx
+++ screens/Profile/ProfileLanding.tsx
@@ -8,6 +8,8 @@ import layoutStyles from '../../styles/layout';
 import profileLandingStyles from '../../styles/screenStyles/profileLanding';
 import { logoutAction } from '../../store/authSlice';
 import { useAppDispatch } from '../../store/hooks';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from "expo-file-system";
 
 interface Props {
   navigation: any
@@ -21,15 +23,56 @@ const ProfileLanding: React.FC<Props> = ({ navigation }) => {
     // Refresh functions here
     setRefreshing(false);
   }
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      base64: true,
+      aspect: [3, 3],
+      quality: 0,
+    });
+
+    if (result.canceled === false) {
+      console.log(result);
+      // const imageExt = result.uri.split('.').pop();
+      // const imageFileName = currentUser.email.split('@')[0];
+      // const base64 = await FileSystem.readAsStringAsync(result.uri, {
+      //   encoding: FileSystem.EncodingType.Base64,
+      // });
+      // const buff = Buffer.from(base64, "base64");
+      // const preAuthPostUrl = await postPresignedUrl({ fileName: imageFileName, fileType: `${result.type}/${imageExt}`}).then((response) => response).catch((e) => {
+      //   return e;
+      // });
+      // if (preAuthPostUrl.status === 201 && preAuthPostUrl.data) {
+      //   // await putImageOnS3(preAuthPostUrl.data, buff, `${result.type}/${imageExt}`).catch((e) => console.log(e));
+      //   // dispatch(updateCurrentUserAsync({ id: currentUser.id, updateBody: {
+      //   //   profilePhoto: imageFileName,
+      //   // }}))
+      // }
+    };
+  };
   return (
     <View style={[layoutStyles.screenContainer]}>
       <ScrollView showsVerticalScrollIndicator={false}  refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
         <View style={[layoutStyles.m_1, layoutStyles.mt_3, layoutStyles.mb_3]}>
           <View style={[imageStyles.profileImageContainer]}>
-            <Image
-              source={require("../../assets/profilePhotos/testProfile.jpg")}
-              style={[imageStyles.profileImage]}
-            />
+            <View>
+              <Image
+                source={require("../../assets/profilePhotos/testProfile.jpg")}
+                style={[imageStyles.profileImage]}
+              />
+              <Pressable
+                onPress={pickImage}
+                style={[{ position: 'absolute', right: 0, bottom: 0, backgroundColor: 'white', borderRadius: 10}]}
+              >
+                <Image
+                  source={require("../../assets/icons/Plus.png")}
+                  style={[{ height: 35, width: 35, resizeMode: 'contain'}]}
+                />
+              </Pressable>
+            </View>
           </View>
           <CustomText h2 bold center>
             Chad Laflamme
