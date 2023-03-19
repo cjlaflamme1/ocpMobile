6303f5f380cbbc085ba5385ee8aceeec6ed6a9f3
cjlaflamme1 <chadlaflamme@gmail.com>
2023-03-19 17:13:20 -0400

    join group, fix login issue

--- screens/Search/SearchLanding.tsx
+++ screens/Search/SearchLanding.tsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { View, Image, ScrollView, RefreshControl, Pressable, TextInput } from 'react-native';
 import CustomText from '../../components/CustomText';
 import GroupCard from '../../components/GroupCard';
-import { getAllGroupsAsync } from '../../store/groupSlice';
+import PreviewGroupModal from '../../components/groups/PreviewGroupModal';
+import { getAllGroupsAsync, getAllUserGroupsAsync, updateGroupAsync } from '../../store/groupSlice';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import globalStyles from '../../styles/global';
 import layoutStyles from '../../styles/layout';
@@ -14,11 +15,14 @@ interface Props {
 
 const SearchLanding: React.FC<Props> = ({ navigation }) => {
   const [refreshing, setRefreshing] = useState(false);
+  const [selectedPreview, setSelectedPreview] = useState('');
   const dispatch = useAppDispatch();
   const currentState = useAppSelector((state) => ({
     groupState: state.groupState,
+    userState: state.userState,
   }));
   const { searchForGroups } = currentState.groupState;
+  const { currentUser } = currentState.userState;
 
   useEffect(() => {
     if (searchForGroups && searchForGroups.count <= 0) {
@@ -30,6 +34,11 @@ const SearchLanding: React.FC<Props> = ({ navigation }) => {
       }));
     }
   }, [navigation]);
+
+  if (!currentUser) {
+    return (<View />);
+  }
+
   const onRefresh = async () => {
     setRefreshing(true);
     await dispatch(getAllGroupsAsync({
@@ -40,6 +49,29 @@ const SearchLanding: React.FC<Props> = ({ navigation }) => {
     }))
     setRefreshing(false);
   }
+
+  const joinGroup = async (groupid: string, userid: string) => {
+    await dispatch(updateGroupAsync({
+      id: groupid,
+      body: {
+        addingUserIds: [userid],
+      },
+    }));
+    dispatch(getAllUserGroupsAsync({
+      pagination: {
+        take: 8,
+        skip: 0,
+      }
+    }))
+    dispatch(getAllGroupsAsync({
+      pagination: {
+        take: 8,
+        skip: 0,
+      }
+    }));
+    setSelectedPreview('');
+  }
+
   return (
     <View style={[layoutStyles.screenContainer]}>
       <ScrollView showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
@@ -69,21 +101,46 @@ const SearchLanding: React.FC<Props> = ({ navigation }) => {
           />
         </View>
         <View style={[layoutStyles.mt_2]}>
-          <CustomText style={[globalStyles.mutedText]}>{!searchForGroups.groups || searchForGroups.groups.length <= 0 ? 'No' : searchForGroups.groups.length} results found.</CustomText>
+          <CustomText style={[globalStyles.mutedText]}>{!searchForGroups || !searchForGroups.groups || searchForGroups.groups.length <= 0 ? 'No' : searchForGroups.groups.length} results found.</CustomText>
         </View>
         <View>
           {
             searchForGroups &&
             searchForGroups.groups &&
             searchForGroups.groups.length > 0 ?
-            searchForGroups.groups.map((group) => (
-              <Pressable key={`userGroupCard-${group.id}`} onPress={() => console.log('review group screen, whatev')}>
+            searchForGroups.groups
+              // .filter((group) => {
+              //   let include = true;
+              //   if (group.users && group.users.length > 0) {
+              //     const found = group.users.find((u) => u.id === currentUser.id);
+              //     if (found) {
+              //       include = false;
+              //     }
+              //   }
+              //   if (group.groupAdmins && group.groupAdmins.length > 0) {
+              //     const found = group.groupAdmins.find((u) => u.id === currentUser.id);
+              //     if (found) {
+              //       include = false;
+              //     }
+              //   }
+              //   if (include) {
+              //     return group;
+              //   }
+              // })
+              .map((group) => (
+              <Pressable key={`userGroupCard-${group.id}`} onPress={() => setSelectedPreview(group.id)}>
                 <GroupCard
                   groupTitle={group.title}
                   numberOfMembers={group.users ? group.users.length : 0}
                   imageSource={group.imageGetUrl ? {
                     uri: group.imageGetUrl
                   } : require('../../assets/150x150.png')}
+                />
+                <PreviewGroupModal
+                  group={group}
+                  isVisible={selectedPreview === group.id}
+                  closeModal={() => setSelectedPreview('')}
+                  acceptAction={() => joinGroup(group.id, currentUser.id)}
                 />
               </Pressable>
               )) : (
