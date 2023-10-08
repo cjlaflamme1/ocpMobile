3c521ed837e6513481dce85493b9bf9d633caf70
cjlaflamme1 <chadlaflamme@gmail.com>
2023-10-08 10:26:43 -0400

    final changes

--- api/api.ts
+++ api/api.ts
@@ -3,9 +3,9 @@ import * as SecureStore from 'expo-secure-store';
 import { Platform } from 'react-native';
 
 const api = axios.create({
-  // baseURL: 'https://prod.outdoorcommunityproject.com',
+  baseURL: 'https://prod.outdoorcommunityproject.com',
   // baseURL: 'https://staging.outdoorcommunityproject.com',
-  baseURL: Platform.OS === 'ios' ? 'http://localhost:3000' : 'http://10.0.2.2:3000',
+  // baseURL: Platform.OS === 'ios' ? 'http://localhost:3000' : 'http://10.0.2.2:3000',
   headers: {
     'Content-Type': 'application/json',
   },
